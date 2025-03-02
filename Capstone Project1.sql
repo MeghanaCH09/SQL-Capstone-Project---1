d7862b15b71a7031e82f6584fa2f6ef0490377e3
MeghanaCH09 <meghanacheruvu@gmial.com>
2025-03-02 10:48:23 +0200

    Capstone Project

--- Capstone Project1.sql
+++ Capstone Project1.sql
@@ -45,24 +45,59 @@ VALUES
 CREATE TABLE IF NOT EXISTS Orders(
 Order_No INTEGER PRIMARY KEY,
 Order_City TEXT,
+Purchase_Amt REAL,
 Salesman_Id INTEGER, 
 Customer_Id INTEGER,
 FOREIGN KEY (Salesman_Id) REFERENCES Salesman (Salesman_Id),
 FOREIGN KEY (Customer_Id) REFERENCES Customer (Customer_Id));
 
-INSERT INTO Orders(Order_No, Order_City, Salesman_Id, Customer_Id)
+INSERT INTO Orders(Order_No, Order_City, Purchase_Amt, Salesman_Id, Customer_Id)
 VALUES
-(50, 'New York', 220, 350),
-(51, 'Washington', 221, 351),
-(52, 'Sydney', 222, 352),
-(53, 'Melbourne', 223, 353),
-(54, 'Toronto', 224, 354),
-(55, 'Moscow', 225, 355),
-(56, 'Tokyo', 226, 356),
-(57, 'Seoul', 227, 357),
-(58, 'Dheli', 228, 358),
-(59, 'London', 229, 359);
+(50, 'New York', 300 ,220, 350),
+(51, 'Washington', 333, 221, 351),
+(52, 'Sydney', 500, 222, 352),
+(53, 'Melbourne', 421, 223, 353),
+(54, 'Toronto', 654, 224, 354),
+(55, 'Moscow', 639, 225, 355),
+(56, 'Tokyo', 354, 226, 356),
+(57, 'Seoul', 765, 227, 357),
+(58, 'Dheli', 987, 228, 358),
+(59, 'London', 543, 229, 359);
 
 SELECT Customer.Customer_Name, Salesman.Name, Salesman.Country
 FROM Customer
 JOIN Salesman ON Customer.Customer_Country = Salesman.Country;
+
+SELECT Customer.Customer_Name, Salesman.Name
+FROM Customer
+JOIN Salesman ON Customer.Salesman_Id = Salesman.Salesman_Id;
+
+SELECT Orders.Order_No, Customer.Customer_Name, Orders.Customer_Id, Orders.Salesman_Id
+FROM Orders
+JOIN Customer ON Orders.Customer_Id = Orders.Customer_Id
+JOIN Salesman ON Orders.Salesman_Id = Salesman.Salesman_Id
+WHERE Customer.Customer_Country <> Salesman.Country;
+
+SELECT Orders.Order_No, Customer.Customer_Name
+FROM Orders
+JOIN Customer ON Orders.Customer_Id = Customer.Customer_Id;
+
+SELECT Customer.Customer_Name AS "Customer", Customer.Customer_Grade AS "Grade"
+FROM Customer
+WHERE Costumer.Customer_Grade IS NOT NULL;
+
+SELECT Customer.Customer_Name AS "Customer",
+       Customer.Country_Country AS "Country",
+       Salesman.Name AS "Salesman",
+       Salesman.Commission
+FROM Customer
+JOIN Salesman ON Customer.Salesman_Id = Salesman.Salesman_Id
+WHERE Salesman.Commission BETWEEN 0.12 AND 0.14;
+
+SELECT Orders.Order_No, Customer.Customer_Name, 
+       Salesman.Commission AS "Commission%",
+       Orders.Purchase_Amt * Salesman.Commission AS "Commission"
+FROM Orders
+JOIN Salesman ON Orders.Salesman_Id = Salesman.Salesman_Id
+JOIN Customer ON Orders.Customer_Id = Customer.Customer_Id
+WHERE Customer.Customer_Grade >= 200;
